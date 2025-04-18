9881bf30cef1b6ecb1f0abadeacd574db0b2fd3c
ry <ryr4242@gmail.com>
2025-04-18 00:32:58 -0500

    Always show OTS to spectators (#10767)

--- sim/battle.ts
+++ sim/battle.ts
@@ -3112,7 +3112,7 @@ export class Battle {
 		return team as PokemonSet[];
 	}
 
-	showOpenTeamSheets(hideFromSpectators = false) {
+	showOpenTeamSheets() {
 		if (this.turn !== 0) return;
 		for (const side of this.sides) {
 			const team = side.pokemon.map(pokemon => {
@@ -3146,13 +3146,8 @@ export class Battle {
 				}
 				return newSet;
 			});
-			if (hideFromSpectators) {
-				for (const s of this.sides) {
-					this.addSplit(s.id, ['showteam', side.id, Teams.pack(team)]);
-				}
-			} else {
-				this.add('showteam', side.id, Teams.pack(team));
-			}
+
+			this.add('showteam', side.id, Teams.pack(team));
 		}
 	}
 
