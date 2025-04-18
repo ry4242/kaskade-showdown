976d2335d90b9929da3b93e6212d2fe0cde1e742
ry <ryr4242@gmail.com>
2025-04-18 01:22:48 -0500

    Gen II: Move HP berries' activations after Reflect, etc. (#10836)

--- data/items.ts
+++ data/items.ts
@@ -7397,7 +7397,7 @@ export const Items: import('../sim/dex-items').ItemDataTable = {
 			basePower: 80,
 			type: "Poison",
 		},
-		onResidualOrder: 5,
+		onResidualOrder: 10,
 		onResidual(pokemon) {
 			if (pokemon.hp <= pokemon.maxhp / 2) {
 				pokemon.eatItem();
@@ -7463,7 +7463,7 @@ export const Items: import('../sim/dex-items').ItemDataTable = {
 			basePower: 80,
 			type: "Psychic",
 		},
-		onResidualOrder: 5,
+		onResidualOrder: 10,
 		onResidual(pokemon) {
 			if (pokemon.hp <= pokemon.maxhp / 2) {
 				pokemon.eatItem();
