60986dbdbf8b4ff56c04be81ee4e555aa3fafbf8
ry <ryr4242@gmail.com>
2025-04-18 21:06:10 -0500

    /ds: Param to expand collapsible datasearches by default (#11023)
    * Reimplement all as a param to automatically expand nonbroadcast results.
    
    * Tests ensuring datasearches don't expand when broadcast with all

--- test/server/chat-plugins/datasearch.js
+++ test/server/chat-plugins/datasearch.js
@@ -115,14 +115,14 @@ describe("Datasearch Plugin", () => {
 	it('should include Pokemon capable of learning normally unobtainable moves under the provided rulsets', () => {
 		const cmd = 'ds';
 		const target = 'mod=gen8, rule=stabmonsmovelegality, rule=alphabetcupmovelegality, calm mind, boomburst, steel';
-		const search = datasearch.testables.runDexsearch(target, cmd, true, `/${cmd} ${target}`);
+		const search = datasearch.testables.runDexsearch(target, cmd, `/${cmd} ${target}`);
 		assert(search.reply.includes('Metagross'));
 	});
 
 	it('should exclude Pokemon capable of learning moves under normal circumstances or the provided rulesets', () => {
 		const cmd = 'ds';
 		const target = 'mod=gen9, rule=stabmonsmovelegality, !wish';
-		const search = datasearch.testables.runDexsearch(target, cmd, true, `/${cmd} ${target}`);
+		const search = datasearch.testables.runDexsearch(target, cmd, `/${cmd} ${target}`);
 		assert.false(search.reply.includes('Alomomola'));
 		assert.false(search.reply.includes('Altaria'));
 	});
@@ -130,7 +130,7 @@ describe("Datasearch Plugin", () => {
 	it('should include only Pokemon allowed by the provided rulesets', () => {
 		const cmd = 'ds';
 		const target = 'mod=gen8, rule=kalospokedex, rule=hoennpokedex, water, ground';
-		const search = datasearch.testables.runDexsearch(target, cmd, true, `/${cmd} ${target}`);
+		const search = datasearch.testables.runDexsearch(target, cmd, `/${cmd} ${target}`);
 		assert(search.reply.includes('Whiscash'));
 		assert.false(search.reply.includes('Swampert'));
 		assert.false(search.reply.includes('Quagsire'));
@@ -140,7 +140,31 @@ describe("Datasearch Plugin", () => {
 	it('should sort Pokemon with modified stats based on rulesets', () => {
 		const cmd = 'ds';
 		const target = 'mod=gen8, rule=reevolutionmod, water, bst desc';
-		const search = datasearch.testables.runDexsearch(target, cmd, true, `/${cmd} ${target}`);
+		const search = datasearch.testables.runDexsearch(target, cmd, `/${cmd} ${target}`);
 		assert(search.reply.includes('Milotic'));
+	});
+
+	it('should collapse results by default when broadcasting a large datasearch with all as a parameter', () => {
+		let cmd = 'ds';
+		let target = 'mod=gen8, water, all';
+		let search = datasearch.testables.runDexsearch(target, cmd, `!${cmd} ${target}`);
+		assert(search.reply.includes('[+]'));
+
+		cmd = 'ms';
+		target = 'mod=gen8, water, all';
+		search = datasearch.testables.runDexsearch(target, cmd, `!${cmd} ${target}`);
+		assert(search.reply.includes('[+]'));
+	});
+
+	it('should expand results by default when not broadcasting a large datasearch with all as a parameter', () => {
+		let cmd = 'ds';
+		let target = 'mod=gen8, water, all';
+		let search = datasearch.testables.runDexsearch(target, cmd, `/${cmd} ${target}`);
+		assert(search.reply.includes('[-]'));
+
+		cmd = 'ms';
+		target = 'mod=gen8, water, all';
+		search = datasearch.testables.runDexsearch(target, cmd, `/${cmd} ${target}`);
+		assert(search.reply.includes('[-]'));
 	});
 });
