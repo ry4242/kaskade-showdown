811d7618a863a3f48bb59cbf587656a316fff687
ry <ryr4242@gmail.com>
2026-03-02 01:31:41 -0600

    march rotation

--- sim/global-types.ts
+++ sim/global-types.ts
@@ -543,6 +543,7 @@ declare namespace RandomTeamsTypes {
 	export interface RandomSet {
 		name: string;
 		species: string;
+		speciesId?: string;
 		gender: string | boolean;
 		moves: string[];
 		ability: string;
